f1016ee408474d422b1b0de0c50c850a81c01b14
harprit <harprits10@gmail.com>
2020-06-02 13:30:19 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./Header";
+import ProductItem from "./ProductItem";
 
-function App() {
+const product = {
+  id: 1,
+  name: "mango",
+};
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div>
+        <ProductItem product={product}></ProductItem>
+      </div>
     </div>
   );
 }
-
-export default App;
